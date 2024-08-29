d7d936cf2c0c7a6acb7f0a53fce5691f375555a2
Bhomik Singh <bhomik009@gmail.com>
2024-08-30 01:06:32 +0530

    url handling

---
+++ handlingURLInGo/go.mod
@@ -0,0 +1,3 @@
+module handlingURLInGo
+
+go 1.23.0
---
+++ handlingURLInGo/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"net/url"
+)
+
+const myurl string = "https://lco.dev:3000/learn?coursename=reactjs&paymentid=ghbj456ghb"
+
+func main() {
+	fmt.Println("How to extract info from URL in GO")
+
+	// parsing the url (Through this we will get all the info from url)
+	result, _ := url.Parse(myurl)
+
+	// fmt.Println(result.Scheme)
+	// fmt.Println(result.Host)
+	// fmt.Println(result.Path)
+	// fmt.Println(result.Port())
+	// fmt.Println(result.RawQuery)
+
+	qparams := result.Query()
+	fmt.Printf("The type of query params are: %T\n", qparams)
+	fmt.Printf("The query params are: %v\n", qparams)
+
+	fmt.Println(qparams["coursename"])
+
+	for _, val := range qparams {
+		fmt.Println("Param is: ", val)
+	}
+
+	// constructing a url
+	partsOfUrl := &url.URL{
+		Scheme:  "https",
+		Host:    "lco.dev",
+		Path:    "/tutcss",
+		RawPath: "user=hitesh",
+	}
+
+	anotherURL := partsOfUrl.String()
+	fmt.Println(anotherURL)
+
+}
